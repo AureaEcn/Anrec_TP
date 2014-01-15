7b65884f7ebe326b546b13c2d882780459e3a63d
Boris LAM <boris.lam@eleves.ec-nantes.fr>
2014-01-15 10:50:06 +0100

    1er commit

---
+++ TP_ANREC/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main( String[] args){
+	System.out.println("Bienvenue dans le programme test pour le TP ANREC");
+System.out.println("Auteurs: LAM Boris, Clement DELILLE");
+
+	
+}
+}
